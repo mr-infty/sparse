576829788cf4b06b854e760ca207d78ab8fffea2
Nicolas Alexander Schmidt <zero@fromzerotoinfinity.xyz>
2021-02-23 13:27:47 +0100

    Adventures in recursion.
    The definition of the parser for the ab language is a bit more verbose
    than one would like it to be. However, it works.
    
    The same is not true for the following, simpler definition (which the
    type checker accepts):
    
        ab = unit | ('a'_P & ab & 'b'_P & ab) | ('b'_P & ab & 'a'_P & ab);
    
    Running this will result in `std::bad_function_call`. What's wrong?

--- ab.cpp
+++ ab.cpp
@@ -14,6 +14,10 @@ Parser ab = [](const iter start, const iter end) {
 	return (unit | (a & ab & b & ab) | (b & ab & a & ab))(start, end);
 };
 
+// Trying to use this parser results in std::bad_function_call.
+// TODO: Fix this.
+Parser ba = unit | ('a'_P & ba & 'b'_P & ba) | ('b'_P & ba & 'a'_P & ba);
+
 int main()
 {
 	string l;
